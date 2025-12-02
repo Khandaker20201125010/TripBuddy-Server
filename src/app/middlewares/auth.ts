6083eb245b1b371faccfa846498d5b090f810132
unknown <prantokih42@gmail.com>
2025-12-03 02:12:09 +0600

    super admin token modified

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,30 +1,29 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
 import { jwtHelper } from "../helper/jwtHelper";
 import config from "../config";
 
 const auth = (...roles: string[]) => {
-    return async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
-        try {
-            const token = req.cookies.get("accessToken");
+  return async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
+    try {
+      const token = req.cookies.accessToken; // <-- fix here
 
-            if (!token) {
-                throw new Error("You are not authorized!")
-            }
+      if (!token) {
+        throw new Error("You are not authorized!");
+      }
 
-            const verifyUser = jwtHelper.verifyToken(token, config.jwt.refresh_secret);
+      const verifyUser = jwtHelper.verifyToken(token, config.jwt.access_secret); // use access_secret
 
-            req.user = verifyUser;
+      req.user = verifyUser;
 
-            if (roles.length && !roles.includes(verifyUser.role)) {
-                throw new Error("You are not authorized!")
-            }
+      if (roles.length && !roles.includes(verifyUser.role)) {
+        throw new Error("You are not authorized!");
+      }
 
-            next();
-        }
-        catch (err) {
-            next(err)
-        }
+      next();
+    } catch (err) {
+      next(err);
     }
-}
+  };
+};
 
 export default auth;
